1214c911e9eb1f80aeb7ccd27fa2747cf1150506
JiaomaWHU <jiaomajiaoma@hotmail.com>
2019-01-04 23:50:39 +0800

    add file

---
+++ Assignment 1: Percolation/Percolation.java
@@ -0,0 +1,13 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation{
+    public Percolation(){
+
+    }
+
+    public static void main(){
+
+    }
+}
